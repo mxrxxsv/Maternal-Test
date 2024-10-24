ca97e4b82cc545d61be19641a7a9e8adbde6092c
mxrxxsv <130885953+mxrxxsv@users.noreply.github.com>
2024-10-24 14:33:23 +0800

    Updating of the theories of labor

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,151 +1,378 @@
+const questions = {
+
+  mechanismOfLabor: [
+    {
+      question:
+        "Fatal head reaches the pelvic floor, the head bends forward onto the chest",
+      options: ["Flexion", "Internal Rotation", "Descent", "Extension"],
+      answer: 0,
+      clues: [],
+    },
+    {
+      question:
+        "The head extends, and the foremost parts of the head, the face and chin, are born.",
+      options: ["Flexion", "Internal Rotation", "Descent", "Extension"],
+      answer: 3,
+      clues: [],
+    },
+    {
+      question:
+        "head rotates from the anteroposterior position it assumed to enter the outlet back to the diagonal or transverse position of the early part of labor",
+      options: [
+        "Expulsion",
+        "Internal Rotation",
+        "External Rotation",
+        "Extension",
+      ],
+      answer: 2,
+      clues: [],
+    },
+    {
+      question:
+        "Once the shoulders are born, the rest of the baby is born easily and smoothly because of its smaller size. This movement, called expulsion, is the end of the pelvic division of labor",
+      options: [
+        "Expulsion",
+        "Internal Rotation",
+        "External Rotation",
+        "Extension",
+      ],
+      answer: 0,
+      clues: [],
+    },
+    {
+      question:
+        "downward movement of the biparietal diameter of the fetal head to within the pelvic inlet. Full descent occurs when the fetal head extrudes beyond the dilated cervix and touches the posterior vaginal floor.",
+      options: ["Expulsion", "Extension", "Descent", "Flexion"],
+      answer: 2,
+      clues: [],
+    },
+    {
+      question:
+        "The head flexes as it touches the pelvic floor, and the occiput rotates to bring the head into the best relationship to the outlet of the pelvis ",
+      options: [
+        "Expulsion",
+        "Internal Rotation",
+        "Descent",
+        "External Rotation",
+      ],
+      answer: 1,
+      clues: [],
+    },
+  ],
 
 
-const questions = [
+
+  theoriesOfLabor: [
     {
-        question: "Fatal head reaches the pelvic floor, the head bends forward onto the chest",
-        options: ["Flexion", "Internal Rotation", "Descent", "Extension"],
-        answer: 0,
-        clues: []
+      question:
+        "Which of the following theories explains the onset of labor as a result of the uterus being over-stretched?",
+      options: [
+        "Progesterone withdrawal",
+        "Uterine distension theory",
+        "Fetal cortisol",
+        "Prostaglandin production",
+      ],
+      answer: 1,
+      clues: [],
     },
     {
-        question: "The head extends, and the foremost parts of the head, the face and chin, are born.",
-        options: ["Flexion", "Internal Rotation", "Descent", "Extension"],
-        answer: 3,
-        clues: []
-    },
-    {
-        question: "head rotates from the anteroposterior position it assumed to enter the outlet back to the diagonal or transverse position of the early part of labor",
-        options: ["Expulsion", "Internal Rotation", "External Rotation", "Extension"],
+        question:
+          "When do labor normally begins when a fetus is sufficiently mature to cope with extrauterine life yet not too large to cause mechanical difficulty with birth",
+        options: [
+          "between 37-44 weeks",
+          "between 33-50 weeks",
+          "between 37-42 weeks",
+          "between 32-49 weeks",
+        ],
         answer: 2,
-        clues: []
-    },
-    {
-        question: "Once the shoulders are born, the rest of the baby is born easily and smoothly because of its smaller size. This movement, called expulsion, is the end of the pelvic division of labor",
-        options: ["Expulsion", "Internal Rotation", "External Rotation", "Extension"],
+        clues: [],
+      },
+      {
+        question:
+          "Results in release of prostaglandins",
+        options: ["Uterine muscle stretching", "Oxytocin stimulation", "Rising fetal cortisol levels", "Placental age"],
         answer: 0,
-        clues: []
-    },
-    {
-        question: "downward movement of the biparietal diameter of the fetal head to within the pelvic inlet. Full descent occurs when the fetal head extrudes beyond the dilated cervix and touches the posterior vaginal floor.",
-        options: ["Expulsion", "Extension", "Descent", "Flexion"],
-        answer: 2,
-        clues: []
-    },
-    {
-        question: "The head flexes as it touches the pelvic floor, and the occiput rotates to bring the head into the best relationship to the outlet of the pelvis ",
-        options: ["Expulsion", "Internal Rotation", "Descent", "External Rotation"],
+        clues: [],
+      },
+      {
+        question:
+          "Stimulates the release of oxytocin from the posterior pituitary",
+        options: ["Uterine muscle stretching", "Oxytocin stimulation", "Rising fetal cortisol levels", "Placental age"],
         answer: 1,
-        clues: []
+        clues: [],
+      },
+      {
+        question:
+          "Triggers contractions at a set point",
+        options: ["Uterine muscle stretching", "Oxytocin stimulation", "Rising fetal cortisol levels", "Placental age"],
+        answer: 3,
+        clues: [],
+      },
+      {
+        question:
+          "Reduces progesterone formation and increases prostaglandin formation",
+        options: ["Uterine muscle stretching", "Oxytocin stimulation", "Rising fetal cortisol levels", "Placental age"],
+        answer: 2,
+        clues: [],
+      },
+      {
+        question:
+          "As progesterone level falls, body fluid is more easily excreted from the body. This increase in urine production can lead to a weight loss between 1 and 3 pounds",
+        options: ["Slight loss of weight", "Oxytocin stimulation", "Rising fetal cortisol levels", "Ripening of the cervix"],
+        answer: 0,
+        clues: [],
+      },
+      {
+        question:
+          "This increase in activity is related to an increase in epinephrine release initiated by a decrease in progesterone produced by the placenta.",
+        options: ["Slight loss of weight", "Increase in Level of Activity", "Rising fetal cortisol levels", "Ripening of the cervix"],
+        answer: 1,
+        clues: [],
+      },
+      {
+        question:
+          "At term, the cervix becomes still softer (described as “butter-soft”), Soft as our lips, (false sign as labor) ",
+        options: ["Oxytocin stimulation", "Uterine muscle stretching", "Ripening of the cervix ", "Placental age"],
+        answer: 2,
+        clues: [],
+      },
+      {
+        question:
+          "woman usually notices extremely strong Braxton Hicks contractions",
+        options: ["Uterine muscle stretching", "Oxytocin stimulation", "Braxston Hicks Contraction", "Placental age"],
+        answer: 2,
+        clues: [],
+      },
+      {
+        question:
+          "Age of viability",
+        options: ["20 weeks or 22 weeks of amenorrhea (WHO)", "22 weeks or 24 weeks of amenorrhea (WHO)", "10 weeks or 22 weeks of amenorrhea (WHO)", "Placental 27 weeks or 32 weeks of amenorrhea (WHO)"],
+        answer: 0,
+        clues: [],
+      },
+      {
+        question:
+          "Extremely Preterm (prone in infection)",
+        options: ["13-28 weeks", "20-27 weeks", "25-28 weeks", "23-28 weeks"],
+        answer: 3,
+        clues: [],
+      },
+      {
+        question:
+          "Preterm (can live outside the uterus)",
+        options: ["less than 256 days", "less than 259 days", "less than 243 days", "less than 250 days"],
+        answer: 1,
+        clues: [],
+      },
+      {
+        question:
+          "Term",
+        options: ["259 days-294 days", "252 days-296 days", "257 days-293 days", "252 days-290 days"],
+        answer: 2,
+        clues: [],
+      },
+      {
+        question:
+          "Post-Term",
+        options: ["259 days", "270 days", "269 days", "294 days"],
+        answer: 3,
+        clues: [],
+      },
+      {
+        question:
+          "route a fetus must travel from the uterus through the cervix and vagina to the external perineum. (a woman pelvis)",
+        options: ["Passage", "Passenger", "Molding", "Station"],
+        answer: 0,
+        clues: [],
+      },
+      {
+        question:
+          "body part of the fetus that has the widest diameter is the head, so this is the part least likely to be able to pass through the pelvic ring. (The fetus)",
+        options: ["Passage", "Passenger", "Molding", "Station"],
+        answer: 1,
+        clues: [],
+      },
+      {
+        question:
+          "is a change in the shape of the fetal skull produced by the force of uterine contractions pressing the vertex of the head against the not-yet-dilated cervix.",
+        options: ["Station", "Engagement", "Molding", "Station"],
+        answer: 2,
+        clues: [],
+      },
+      {
+        question:
+          "refers to the settling of the presenting part of a fetus far enough into the pelvis to be at the level of the ischial spines, a midpoint of the pelvis.",
+        options: ["Station", "Engagement", "Molding", "Station"],
+        answer: 0,
+        clues: [],
+      },
+      {
+        question:
+          "Attitude describes the degree of flexion a fetus assumes during labor or the relation of the fetal parts to each other. A fetus in good attitude is in complete flexion:",
+        options: ["Station", "Engagement", "Fetal Attitude", "Molding"],
+        answer: 2,
+        clues: [],
+      },
+      {
+        question:
+          "means that the widest part of the fetus (the biparietal diameter in a cephalic presentation; the intertrochanteric diameter in a breech presentation",
+        options: ["Descent", "Engagement", "Fetal Lie", "Molding"],
+        answer: 3,
+        clues: [],
+      },
+      {
+        question:
+          "the relationship between the long (cephalocaudal) axis of the fetal body and the long (cephalocaudal) axis of a woman’s body; in other words, whether the fetus is lying in a horizontal (transverse) or a vertical (longitudinal) position.",
+        options: ["Descent", "Fetal Attitude", "Fetal Lie", "Molding"],
+        answer: 2,
+        clues: [],
+      },
+      
+  ],
+
+
+
+  fetalPresentation: [
+    {
+      question:
+        "Which fetal presentation is most favorable for a natural vaginal delivery?",
+      options: ["Breech", "Transverse", "Vertex", "Face"],
+      answer: 2,
+      clues: [],
     },
-   
-];
+    
+  ],
+
+
+
+  newbornAppearance: [
+    {
+      question:
+        "What is the term for the white, waxy substance that covers a newborn's skin?",
+      options: ["Meconium", "Lanugo", "Vernix caseosa", "Amniotic fluid"],
+      answer: 2,
+      clues: [],
+    },
+
+  ],
+};
 
 let currentQuestion = 0;
 let score = 0;
 let level = 0;
 let timer;
+let selectedQuestions = [];
+
+function loadTopic(topic) {
+  currentTopic = topic;
+  selectedQuestions = [...questions[topic]];
+  startGame();
+}
 
 function displayQuestion() {
-    const currentQ = questions[currentQuestion];
-    document.getElementById('question').textContent = currentQ.question;
-    const clueImagesContainer = document.getElementById('clueImages');
-    clueImagesContainer.innerHTML = '';
-    currentQ.clues.forEach(clue => {
-        const clueImage = document.createElement('img');
-        clueImage.src = clue;
-        clueImage.classList.add('clue-image');
-        clueImagesContainer.appendChild(clueImage);
-    });
-    const optionsContainer = document.getElementById('options');
-    optionsContainer.innerHTML = '';
-    currentQ.options.forEach((opt, idx) => {
-        const optElement = document.createElement('div');
-        optElement.classList.add('option');
-        optElement.textContent = `${String.fromCharCode(97 + idx)}. ${opt}`;
-        optElement.setAttribute('data-option', idx);
-        optElement.addEventListener('click', selectOption);
-        optionsContainer.appendChild(optElement);
-    });
+  const currentQ = selectedQuestions[currentQuestion];
+  document.getElementById("question").textContent = currentQ.question;
+  const clueImagesContainer = document.getElementById("clueImages");
+  clueImagesContainer.innerHTML = "";
+  currentQ.clues.forEach((clue) => {
+    const clueImage = document.createElement("img");
+    clueImage.src = clue;
+    clueImage.classList.add("clue-image");
+    clueImagesContainer.appendChild(clueImage);
+  });
+  const optionsContainer = document.getElementById("options");
+  optionsContainer.innerHTML = "";
+  currentQ.options.forEach((opt, idx) => {
+    const optElement = document.createElement("div");
+    optElement.classList.add("option");
+    optElement.textContent = `${String.fromCharCode(97 + idx)}. ${opt}`;
+    optElement.setAttribute("data-option", idx);
+    optElement.addEventListener("click", selectOption);
+    optionsContainer.appendChild(optElement);
+  });
 }
 
 function selectOption(event) {
-    clearInterval(timer);
-    const selectedOption = parseInt(event.target.getAttribute('data-option'));
-    const correctOption = questions[currentQuestion].answer;
-    const options = document.querySelectorAll('.option');
-    options.forEach(opt => opt.removeEventListener('click', selectOption));
-    options[correctOption].classList.add('correct');
-    if (selectedOption === correctOption) {
-        event.target.classList.add('correct');
-        score += 10;
-    } else {
-        event.target.classList.add('incorrect');
-    }
-    document.getElementById('currentScore').textContent = `Score: ${score}`;
-    setTimeout(nextQuestion, 2000);
+  clearInterval(timer);
+  const selectedOption = parseInt(event.target.getAttribute("data-option"));
+  const correctOption = selectedQuestions[currentQuestion].answer;
+  const options = document.querySelectorAll(".option");
+  options.forEach((opt) => opt.removeEventListener("click", selectOption));
+  options[correctOption].classList.add("correct");
+  if (selectedOption === correctOption) {
+    event.target.classList.add("correct");
+    score += 10;
+  } else {
+    event.target.classList.add("incorrect");
+  }
+  document.getElementById("currentScore").textContent = `Score: ${score}`;
+  setTimeout(nextQuestion, 2000);
 }
 
 function nextQuestion() {
-    currentQuestion++;
-    if (currentQuestion < questions.length) {
-        document.getElementById('testNumber').textContent = `${(score + 10) / 10}/6`;
-        displayQuestion();
-        startTimer();
-    } else {
-        endQuiz();
-    }
+  currentQuestion++;
+  if (currentQuestion < selectedQuestions.length) {
+    document.getElementById("testNumber").textContent = `${score / 10}`+ `/` + selectedQuestions.length;
+    displayQuestion();
+    startTimer();
+  } else {
+    endQuiz();
+  }
 }
 
 function startTimer() {
-    let timeLeft = 20;
-    document.getElementById('time').textContent = timeLeft;
-    timer = setInterval(() => {
-        timeLeft--;
-        document.getElementById('time').textContent = timeLeft;
-        if (timeLeft === 0) {
-            clearInterval(timer);
-            document.querySelectorAll('.option').forEach(opt => opt.removeEventListener('click', selectOption));
-            document.getElementById('nextButton').style.display = 'block';
-        }
-    }, 1000);
+  let timeLeft = 20;
+  document.getElementById("time").textContent = timeLeft;
+  timer = setInterval(() => {
+    timeLeft--;
+    document.getElementById("time").textContent = timeLeft;
+    if (timeLeft === 0) {
+      clearInterval(timer);
+      document
+        .querySelectorAll(".option")
+        .forEach((opt) => opt.removeEventListener("click", selectOption));
+      document.getElementById("nextButton").style.display = "block";
+    }
+  }, 1000);
 }
 
 function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]]; 
-    }
-    return array;
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
 function startGame() {
-    document.getElementById('instruction').style.display = 'none';
-    document.getElementById('game').style.display = 'block';
-    shuffle(questions);
-    displayQuestion();
-    startTimer();
+  document.getElementById("instruction").style.display = "none";
+  document.getElementById("game").style.display = "block";
+  shuffle(selectedQuestions);
+  displayQuestion();
+  startTimer();
 }
 
-
 function endQuiz() {
-    document.getElementById('scoreTable').style.display = 'block';
-    document.getElementById('timer').style.display = 'none';
-    document.getElementById('questionBox').innerHTML = `<h2> ♡ Congratulations! Quiz completed! ♡ </h2><p>Your final score is: ${score}</p>`;
-    document.getElementById('nextButton').style.display = 'none';
-    document.getElementById('.options-container').style.display = 'none';
-    document.getElementById('options').style.display = 'none';
-    document.getElementById('resetButton').style.display = 'block';
-    document.getElementById('scoreTable').style.display = 'block';
+  document.getElementById("scoreTable").style.display = "block";
+  document.getElementById("timer").style.display = "none";
+  document.getElementById(
+    "questionBox"
+  ).innerHTML = `<h2> ♡ Congratulations! Test completed! ♡ </h2><p>Your final score is: ${score}</p>`;
+  document.getElementById("nextButton").style.display = "none";
+  document.getElementById(".options-container").style.display = "none";
+  document.getElementById("options").style.display = "none";
+  document.getElementById("resetButton").style.display = "block";
+  document.getElementById("scoreTable").style.display = "block";
 }
 
 function resetGame() {
-    currentQuestion = 0;
-    score = 0;
-    document.getElementById('testNumber').textContent = `${currentQuestion}/20`;
-    document.getElementById('currentScore').textContent = `Score: ${score}`;
-    document.getElementById('instruction').style.display = 'block';
-    document.getElementById('game').style.display = 'none';
-    document.getElementById('resetButton').style.display = 'none';
-    document.getElementById('scoreTable').style.display = 'none';
-    clearInterval(timer);
+  currentQuestion = 0;
+  score = 0;
+  document.getElementById("testNumber").textContent = `${currentQuestion}/20`;
+  document.getElementById("currentScore").textContent = `Score: ${score}`;
+  document.getElementById("instruction").style.display = "block";
+  document.getElementById("game").style.display = "none";
+  document.getElementById("resetButton").style.display = "none";
+  document.getElementById("scoreTable").style.display = "none";
+  clearInterval(timer);
 }
