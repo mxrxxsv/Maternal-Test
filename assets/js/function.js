7b034c0758d12df2de42de2b050c3b6da6d18f57
mxrxxsv <130885953+mxrxxsv@users.noreply.github.com>
2024-10-24 14:54:22 +0800

    Update fetalposition

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,5 +1,4 @@
 const questions = {
-
   mechanismOfLabor: [
     {
       question:
@@ -60,8 +59,6 @@ const questions = {
     },
   ],
 
-
-
   theoriesOfLabor: [
     {
       question:
@@ -76,171 +73,257 @@ const questions = {
       clues: [],
     },
     {
-        question:
-          "When do labor normally begins when a fetus is sufficiently mature to cope with extrauterine life yet not too large to cause mechanical difficulty with birth",
-        options: [
-          "between 37-44 weeks",
-          "between 33-50 weeks",
-          "between 37-42 weeks",
-          "between 32-49 weeks",
-        ],
-        answer: 2,
-        clues: [],
-      },
-      {
-        question:
-          "Results in release of prostaglandins",
-        options: ["Uterine muscle stretching", "Oxytocin stimulation", "Rising fetal cortisol levels", "Placental age"],
-        answer: 0,
-        clues: [],
-      },
-      {
-        question:
-          "Stimulates the release of oxytocin from the posterior pituitary",
-        options: ["Uterine muscle stretching", "Oxytocin stimulation", "Rising fetal cortisol levels", "Placental age"],
-        answer: 1,
-        clues: [],
-      },
-      {
-        question:
-          "Triggers contractions at a set point",
-        options: ["Uterine muscle stretching", "Oxytocin stimulation", "Rising fetal cortisol levels", "Placental age"],
-        answer: 3,
-        clues: [],
-      },
-      {
-        question:
-          "Reduces progesterone formation and increases prostaglandin formation",
-        options: ["Uterine muscle stretching", "Oxytocin stimulation", "Rising fetal cortisol levels", "Placental age"],
-        answer: 2,
-        clues: [],
-      },
-      {
-        question:
-          "As progesterone level falls, body fluid is more easily excreted from the body. This increase in urine production can lead to a weight loss between 1 and 3 pounds",
-        options: ["Slight loss of weight", "Oxytocin stimulation", "Rising fetal cortisol levels", "Ripening of the cervix"],
-        answer: 0,
-        clues: [],
-      },
-      {
-        question:
-          "This increase in activity is related to an increase in epinephrine release initiated by a decrease in progesterone produced by the placenta.",
-        options: ["Slight loss of weight", "Increase in Level of Activity", "Rising fetal cortisol levels", "Ripening of the cervix"],
-        answer: 1,
-        clues: [],
-      },
-      {
-        question:
-          "At term, the cervix becomes still softer (described as “butter-soft”), Soft as our lips, (false sign as labor) ",
-        options: ["Oxytocin stimulation", "Uterine muscle stretching", "Ripening of the cervix ", "Placental age"],
-        answer: 2,
-        clues: [],
-      },
-      {
-        question:
-          "woman usually notices extremely strong Braxton Hicks contractions",
-        options: ["Uterine muscle stretching", "Oxytocin stimulation", "Braxston Hicks Contraction", "Placental age"],
-        answer: 2,
-        clues: [],
-      },
-      {
-        question:
-          "Age of viability",
-        options: ["20 weeks or 22 weeks of amenorrhea (WHO)", "22 weeks or 24 weeks of amenorrhea (WHO)", "10 weeks or 22 weeks of amenorrhea (WHO)", "Placental 27 weeks or 32 weeks of amenorrhea (WHO)"],
-        answer: 0,
-        clues: [],
-      },
-      {
-        question:
-          "Extremely Preterm (prone in infection)",
-        options: ["13-28 weeks", "20-27 weeks", "25-28 weeks", "23-28 weeks"],
-        answer: 3,
-        clues: [],
-      },
-      {
-        question:
-          "Preterm (can live outside the uterus)",
-        options: ["less than 256 days", "less than 259 days", "less than 243 days", "less than 250 days"],
-        answer: 1,
-        clues: [],
-      },
-      {
-        question:
-          "Term",
-        options: ["259 days-294 days", "252 days-296 days", "257 days-293 days", "252 days-290 days"],
-        answer: 2,
-        clues: [],
-      },
-      {
-        question:
-          "Post-Term",
-        options: ["259 days", "270 days", "269 days", "294 days"],
-        answer: 3,
-        clues: [],
-      },
-      {
-        question:
-          "route a fetus must travel from the uterus through the cervix and vagina to the external perineum. (a woman pelvis)",
-        options: ["Passage", "Passenger", "Molding", "Station"],
-        answer: 0,
-        clues: [],
-      },
-      {
-        question:
-          "body part of the fetus that has the widest diameter is the head, so this is the part least likely to be able to pass through the pelvic ring. (The fetus)",
-        options: ["Passage", "Passenger", "Molding", "Station"],
-        answer: 1,
-        clues: [],
-      },
-      {
-        question:
-          "is a change in the shape of the fetal skull produced by the force of uterine contractions pressing the vertex of the head against the not-yet-dilated cervix.",
-        options: ["Station", "Engagement", "Molding", "Station"],
-        answer: 2,
-        clues: [],
-      },
-      {
-        question:
-          "refers to the settling of the presenting part of a fetus far enough into the pelvis to be at the level of the ischial spines, a midpoint of the pelvis.",
-        options: ["Station", "Engagement", "Molding", "Station"],
-        answer: 0,
-        clues: [],
-      },
-      {
-        question:
-          "Attitude describes the degree of flexion a fetus assumes during labor or the relation of the fetal parts to each other. A fetus in good attitude is in complete flexion:",
-        options: ["Station", "Engagement", "Fetal Attitude", "Molding"],
-        answer: 2,
-        clues: [],
-      },
-      {
-        question:
-          "means that the widest part of the fetus (the biparietal diameter in a cephalic presentation; the intertrochanteric diameter in a breech presentation",
-        options: ["Descent", "Engagement", "Fetal Lie", "Molding"],
-        answer: 3,
-        clues: [],
-      },
-      {
-        question:
-          "the relationship between the long (cephalocaudal) axis of the fetal body and the long (cephalocaudal) axis of a woman’s body; in other words, whether the fetus is lying in a horizontal (transverse) or a vertical (longitudinal) position.",
-        options: ["Descent", "Fetal Attitude", "Fetal Lie", "Molding"],
-        answer: 2,
-        clues: [],
-      },
-      
+      question:
+        "When do labor normally begins when a fetus is sufficiently mature to cope with extrauterine life yet not too large to cause mechanical difficulty with birth",
+      options: [
+        "between 37-44 weeks",
+        "between 33-50 weeks",
+        "between 37-42 weeks",
+        "between 32-49 weeks",
+      ],
+      answer: 2,
+      clues: [],
+    },
+    {
+      question: "Results in release of prostaglandins",
+      options: [
+        "Uterine muscle stretching",
+        "Oxytocin stimulation",
+        "Rising fetal cortisol levels",
+        "Placental age",
+      ],
+      answer: 0,
+      clues: [],
+    },
+    {
+      question:
+        "Stimulates the release of oxytocin from the posterior pituitary",
+      options: [
+        "Uterine muscle stretching",
+        "Oxytocin stimulation",
+        "Rising fetal cortisol levels",
+        "Placental age",
+      ],
+      answer: 1,
+      clues: [],
+    },
+    {
+      question: "Triggers contractions at a set point",
+      options: [
+        "Uterine muscle stretching",
+        "Oxytocin stimulation",
+        "Rising fetal cortisol levels",
+        "Placental age",
+      ],
+      answer: 3,
+      clues: [],
+    },
+    {
+      question:
+        "Reduces progesterone formation and increases prostaglandin formation",
+      options: [
+        "Uterine muscle stretching",
+        "Oxytocin stimulation",
+        "Rising fetal cortisol levels",
+        "Placental age",
+      ],
+      answer: 2,
+      clues: [],
+    },
+    {
+      question:
+        "As progesterone level falls, body fluid is more easily excreted from the body. This increase in urine production can lead to a weight loss between 1 and 3 pounds",
+      options: [
+        "Slight loss of weight",
+        "Oxytocin stimulation",
+        "Rising fetal cortisol levels",
+        "Ripening of the cervix",
+      ],
+      answer: 0,
+      clues: [],
+    },
+    {
+      question:
+        "This increase in activity is related to an increase in epinephrine release initiated by a decrease in progesterone produced by the placenta.",
+      options: [
+        "Slight loss of weight",
+        "Increase in Level of Activity",
+        "Rising fetal cortisol levels",
+        "Ripening of the cervix",
+      ],
+      answer: 1,
+      clues: [],
+    },
+    {
+      question:
+        "At term, the cervix becomes still softer (described as “butter-soft”), Soft as our lips, (false sign as labor) ",
+      options: [
+        "Oxytocin stimulation",
+        "Uterine muscle stretching",
+        "Ripening of the cervix ",
+        "Placental age",
+      ],
+      answer: 2,
+      clues: [],
+    },
+    {
+      question:
+        "woman usually notices extremely strong Braxton Hicks contractions",
+      options: [
+        "Uterine muscle stretching",
+        "Oxytocin stimulation",
+        "Braxston Hicks Contraction",
+        "Placental age",
+      ],
+      answer: 2,
+      clues: [],
+    },
+    {
+      question: "Age of viability",
+      options: [
+        "20 weeks or 22 weeks of amenorrhea (WHO)",
+        "22 weeks or 24 weeks of amenorrhea (WHO)",
+        "10 weeks or 22 weeks of amenorrhea (WHO)",
+        "Placental 27 weeks or 32 weeks of amenorrhea (WHO)",
+      ],
+      answer: 0,
+      clues: [],
+    },
+    {
+      question: "Extremely Preterm (prone in infection)",
+      options: ["13-28 weeks", "20-27 weeks", "25-28 weeks", "23-28 weeks"],
+      answer: 3,
+      clues: [],
+    },
+    {
+      question: "Preterm (can live outside the uterus)",
+      options: [
+        "less than 256 days",
+        "less than 259 days",
+        "less than 243 days",
+        "less than 250 days",
+      ],
+      answer: 1,
+      clues: [],
+    },
+    {
+      question: "Term",
+      options: [
+        "259 days-294 days",
+        "252 days-296 days",
+        "257 days-293 days",
+        "252 days-290 days",
+      ],
+      answer: 2,
+      clues: [],
+    },
+    {
+      question: "Post-Term",
+      options: ["259 days", "270 days", "269 days", "294 days"],
+      answer: 3,
+      clues: [],
+    },
+    {
+      question:
+        "route a fetus must travel from the uterus through the cervix and vagina to the external perineum. (a woman pelvis)",
+      options: ["Passage", "Passenger", "Molding", "Station"],
+      answer: 0,
+      clues: [],
+    },
+    {
+      question:
+        "body part of the fetus that has the widest diameter is the head, so this is the part least likely to be able to pass through the pelvic ring. (The fetus)",
+      options: ["Passage", "Passenger", "Molding", "Station"],
+      answer: 1,
+      clues: [],
+    },
+    {
+      question:
+        "is a change in the shape of the fetal skull produced by the force of uterine contractions pressing the vertex of the head against the not-yet-dilated cervix.",
+      options: ["Station", "Engagement", "Molding", "Station"],
+      answer: 2,
+      clues: [],
+    },
+    {
+      question:
+        "refers to the settling of the presenting part of a fetus far enough into the pelvis to be at the level of the ischial spines, a midpoint of the pelvis.",
+      options: ["Station", "Engagement", "Molding", "Station"],
+      answer: 0,
+      clues: [],
+    },
+    {
+      question:
+        "Attitude describes the degree of flexion a fetus assumes during labor or the relation of the fetal parts to each other. A fetus in good attitude is in complete flexion:",
+      options: ["Station", "Engagement", "Fetal Attitude", "Molding"],
+      answer: 2,
+      clues: [],
+    },
+    {
+      question:
+        "means that the widest part of the fetus (the biparietal diameter in a cephalic presentation; the intertrochanteric diameter in a breech presentation",
+      options: ["Descent", "Engagement", "Fetal Lie", "Molding"],
+      answer: 3,
+      clues: [],
+    },
+    {
+      question:
+        "the relationship between the long (cephalocaudal) axis of the fetal body and the long (cephalocaudal) axis of a woman’s body; in other words, whether the fetus is lying in a horizontal (transverse) or a vertical (longitudinal) position.",
+      options: ["Descent", "Fetal Attitude", "Fetal Lie", "Molding"],
+      answer: 2,
+      clues: [],
+    },
   ],
-
 
 
   fetalPresentation: [
     {
       question:
-        "Which fetal presentation is most favorable for a natural vaginal delivery?",
-      options: ["Breech", "Transverse", "Vertex", "Face"],
+        "the most frequent type of presentation, occurring as often as 95% of the time. With this type of presentation, the fetal head is the body part that will first contact the cervix.",
+      options: [
+        "Cephalic Presentation",
+        "Breech Presentation",
+        "Shoulder Presentation",
+        "Cute Presentation",
+      ],
+      answer: 0,
+      clues: [],
+    },
+    {
+      question:
+        "a transverse lie, a fetus lies horizontally in the pelvis so that the longest fetal axis is perpendicular to that of the mother. The presenting part is usually one of the shoulders (acromion process), an iliac crest, a hand, or an elbow.",
+      options: [
+        "Cephalic Presentation",
+        "Breech Presentation",
+        "Shoulder Presentation",
+        "Cute Presentation",
+      ],
       answer: 2,
       clues: [],
     },
-    
+    {
+      question:
+        "either the buttocks or the feet are the first body parts that will contact the cervix. Breech presentations occur in approximately 3% of births and are affected by fetal attitude",
+      options: [
+        "Cute Presentation",
+        "Breech Presentation",
+        "Shoulder Presentation",
+        "Cephalic Presentation",
+      ],
+      answer: 1,
+      clues: [],
+    },
+    {
+      question:
+        "What Position for convenience, the maternal pelvis is divided into four quadrants according to the mother’s right and left: (a) right anterior, (b) left anterior, (c) right posterior, and (d) left posterior.",
+      options: [
+        "Fetal Position",
+        "Shoulder Position",
+        "Cute Position",
+        "Cephalic Position",
+      ],
+      answer: 0,
+      clues: [],
+    },
   ],
 
 
@@ -253,7 +336,6 @@ const questions = {
       answer: 2,
       clues: [],
     },
-
   ],
 };
 
@@ -312,7 +394,7 @@ function selectOption(event) {
 function nextQuestion() {
   currentQuestion++;
   if (currentQuestion < selectedQuestions.length) {
-    document.getElementById("testNumber").textContent = `${score / 10}`+ `/` + selectedQuestions.length;
+    document.getElementById("testNumber").textContent = selectedQuestions.length;
     displayQuestion();
     startTimer();
   } else {
@@ -368,7 +450,7 @@ function endQuiz() {
 function resetGame() {
   currentQuestion = 0;
   score = 0;
-  document.getElementById("testNumber").textContent = `${currentQuestion}/20`;
+  document.getElementById("testNumber").textContent = `${selectedQuestions.length}`;
   document.getElementById("currentScore").textContent = `Score: ${score}`;
   document.getElementById("instruction").style.display = "block";
   document.getElementById("game").style.display = "none";
